9a4f2db5020927c05afd99ea44fe5f151417ec79
mpisbuz <mpisbuz@gmail.com>
2025-04-16 00:02:28 +0330

    main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from remain_up import remain_up
 
 remain_up()
 
-bot = telebot.TeleBot("SET TOKEN")
+bot = telebot.TeleBot("7643716966:AAHdfxihRthlxw3lRFDnhicPjcfvFb3Zfck")
 
 def run_php():
   subprocess.call('php -S 0.0.0.0:5000', shell=True)
